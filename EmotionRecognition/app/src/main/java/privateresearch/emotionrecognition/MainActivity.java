a47d83ec6075768538320ddd7e14bfb37f628f28
Y410P\Alex <alex@andoverrobotics.com>
2017-05-20 16:26:50 -0400

    Implemented camera permissions (kind of)

--- EmotionRecognition/app/src/main/java/privateresearch/emotionrecognition/MainActivity.java
+++ EmotionRecognition/app/src/main/java/privateresearch/emotionrecognition/MainActivity.java
@@ -1,11 +1,22 @@
 package privateresearch.emotionrecognition;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private int MY_PERMISSIONS_REQUEST_CAMERA;
+    private static boolean cameraPermission;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +24,89 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** Called when the user presses the "Use Camera" button **/
     public void pictureUseCamera (View view) {
         Intent intent = new Intent(this, TakePhoto.class);
+        /**
+        private boolean checkCameraHardware(Context context) {
+            if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+                // this device has a camera
+                return true;
+            } else {
+                // no camera on this device
+                return false;
+            }
+        }*/
+        checkCameraPermission();
 
-        startActivity(intent);
+        if (cameraPermission == true) {
+            startActivity(intent);
+        }   // Otherwise do nothing
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public void checkCameraPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if (permissionCheck == PERMISSION_GRANTED) {
+            cameraPermission = true;
+            // Here, thisActivity is the current activity
+        } else if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+    }
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                cameraPermission = false;
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
