d994b18a83590cedbd0686d9850fa2592d150050
Y410P\Alex <alex@andoverrobotics.com>
2017-05-20 20:53:58 -0400

    Camera kind of works VERSION NOT STABLE

---
+++ EmotionRecognition/app/src/main/java/privateresearch/emotionrecognition/ExpectedError.java
@@ -0,0 +1,13 @@
+package privateresearch.emotionrecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpectedError extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expected_error);
+    }
+}
